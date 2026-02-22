1bbb52cf780b1b736f608ede6f32d4f2cdaa0cb4
Wilfried <wilfried@lucha.at>
2026-02-22 09:52:54 +0100

    build: configure Jest to ignore e2e tests and correctly transform ES modules in node_modules.

--- jest.config.ts
+++ jest.config.ts
@@ -2,6 +2,8 @@ import type { Config } from 'jest';
 
 const jestConfig: Config = {
   preset: 'jest-preset-angular',
+  testPathIgnorePatterns: ['/node_modules/', '/e2e/'],
+  transformIgnorePatterns: ['node_modules/(?!.*\\.mjs$|@jsverse)'],
   setupFilesAfterEnv: ['<rootDir>/setup-jest.ts'],
 };
 
